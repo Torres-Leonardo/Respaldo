cef5361d93839ab314f4e4c780ed65b3b3eb8af3
Toshiro Inami <toshiro.inami@vallegrande.edu.pe>
2023-06-05 20:25:07 -0500

    o

--- T15_GestionNotas/src/main/java/Plantilla/RowMapper3.java
+++
@@ -1,9 +0,0 @@
-package Plantilla;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import Modelos.ModeloCourse;
-
-public interface RowMapper3<T> {
-	ModeloCourse mapRow(ResultSet rs) throws SQLException;
-	}
